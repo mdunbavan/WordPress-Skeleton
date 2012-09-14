7e4f29f8e36934260a9bbda80741fa7d891cc6eb
Sean Butze <sbutze@gmail.com>
2012-09-14 10:04:04 -0400

    Updated config files & added theme submodule

--- wp-config.php
+++ wp-config.php
@@ -17,7 +17,6 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
 // Custom Content Directory
 // ========================
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
-define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
 
 // ================================================
 // You almost certainly do not want to change these
@@ -42,7 +41,7 @@ define( 'NONCE_SALT',       'put your unique phrase here' );
 // Table prefix
 // Change this if you have multiple installs in the same database
 // ==============================================================
-$table_prefix  = 'wp_';
+$table_prefix  = 'skeleton_';
 
 // ================================
 // Language
